46c1228de9e34d1cce494e3005bed46976be7600
vinicius <vinapro91@gmail.com>
2021-09-10 12:12:33 -0300

    fix: alteração no filtro

--- src/App.js
+++ src/App.js
@@ -20,7 +20,17 @@ function App() {
     const filterProduct = products.filter((el) =>
       el.name.toLowerCase().includes(input.toLowerCase())
     );
-    input !== "" && setProducts([...filterProduct]);
+    input === ""
+      ? setProducts([
+          { id: 1, name: "Hamburguer", category: "Sanduíches", price: 7.99 },
+          { id: 2, name: "X-Burguer", category: "Sanduíches", price: 8.99 },
+          { id: 3, name: "X-Salada", category: "Sanduíches", price: 10.99 },
+          { id: 4, name: "Big Kenzie", category: "Sanduíches", price: 16.99 },
+          { id: 5, name: "Guaraná", category: "Bebidas", price: 4.99 },
+          { id: 6, name: "Coca", category: "Bebidas", price: 4.99 },
+          { id: 7, name: "Fanta", category: "Bebidas", price: 4.99 },
+        ])
+      : setProducts([...filterProduct]);
   };
 
   const handleClick = (productId) => {
