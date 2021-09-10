ea96a3bb7352fa9f5c1be8cd84043557bd3aa726
vinicius <vinapro91@gmail.com>
2021-09-10 14:02:55 -0300

    fix: corrigido filtro e corrigido bug que item filtrado era adicionado duas vezes

--- src/App.js
+++ src/App.js
@@ -15,23 +15,24 @@ function App() {
   ]);
 
   const [currentSale, setCurrentSale] = useState([]);
-
+  const [filtredProducts, setFilteredProducts] = useState([
+    { id: 1, name: "Hamburguer", category: "Sanduíches", price: 7.99 },
+    { id: 2, name: "X-Burguer", category: "Sanduíches", price: 8.99 },
+    { id: 3, name: "X-Salada", category: "Sanduíches", price: 10.99 },
+    { id: 4, name: "Big Kenzie", category: "Sanduíches", price: 16.99 },
+    { id: 5, name: "Guaraná", category: "Bebidas", price: 4.99 },
+    { id: 6, name: "Coca", category: "Bebidas", price: 4.99 },
+    { id: 7, name: "Fanta", category: "Bebidas", price: 4.99 },
+  ]);
   const showProducts = (input) => {
     const filterProduct = products.filter((el) =>
       el.name.toLowerCase().includes(input.toLowerCase())
     );
-    input === ""
-      ? setProducts([
-          { id: 1, name: "Hamburguer", category: "Sanduíches", price: 7.99 },
-          { id: 2, name: "X-Burguer", category: "Sanduíches", price: 8.99 },
-          { id: 3, name: "X-Salada", category: "Sanduíches", price: 10.99 },
-          { id: 4, name: "Big Kenzie", category: "Sanduíches", price: 16.99 },
-          { id: 5, name: "Guaraná", category: "Bebidas", price: 4.99 },
-          { id: 6, name: "Coca", category: "Bebidas", price: 4.99 },
-          { id: 7, name: "Fanta", category: "Bebidas", price: 4.99 },
-        ])
-      : setProducts([...filterProduct]);
+    input !== ""
+      ? setFilteredProducts([...filterProduct])
+      : setFilteredProducts([...products]);
   };
+  console.log(filtredProducts);
 
   const handleClick = (productId) => {
     const filterProduct = products.filter(
@@ -52,7 +53,7 @@ function App() {
         showProducts={showProducts}
         setProducts={setProducts}
       />
-      <MenuContainer handleClick={handleClick} products={products} />
+      <MenuContainer handleClick={handleClick} products={filtredProducts} />
       <div className="subtotal"> Subtotal: R$: {valorTotal.toFixed(2)}</div>
       <Cart currentSale={currentSale} setCurrentSale={setCurrentSale} />
     </div>
